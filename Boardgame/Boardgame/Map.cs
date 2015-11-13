585b6bf99c078b5449c2a327fb7499b15931ff8e
tomrosenlund <tomro196@student.liu.se>
2015-11-13 15:03:06 +0100

    Created Map class.

---
+++ Boardgame/Boardgame/Map.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boardgame
+{
+    class Map
+    {
+
+        private int[,] board = new int[8, 8];
+
+        Map()
+        {
+
+        }
+
+        public int[,] getBoard()
+        {
+            return this.board;
+        }
+
+        public void setBoard(int[,] board)
+        {
+            this.board = board;
+
+        }
+    }
+}
